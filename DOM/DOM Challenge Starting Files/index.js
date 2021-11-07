b7636069fecb7237d85e7ec6cb6994602aad362c
Chen Qiu <qiu.chen1@northeastern.edu>
2021-11-07 11:38:40 -0800

    update DOM

---
+++ DOM/DOM Challenge Starting Files/index.js
@@ -0,0 +1,3 @@
+//var third = document.first.lastElementChild.lastElementChild.lastElementChild;
+document.lastElementChild.lastElementChild.lastElementChild.innerHTML = "Angela"
+//third = "Oh no!"
