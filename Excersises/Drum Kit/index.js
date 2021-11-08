1250a1d372349204953cf7dd7ec8defbeeddac73
Chen Qiu <qiu.chen1@northeastern.edu>
2021-11-08 12:16:48 -0800

    update button function

--- Excersises/Drum Kit/index.js
+++ Excersises/Drum Kit/index.js
@@ -2,9 +2,14 @@
 var allButtons = document.querySelectorAll(".drum");
 
 for (var i = 0; i < allButtons.length; i++) {
-   allButtons[i].addEventListener("click", handleClick);
+   allButtons[i].addEventListener("click", function() {
+     //console.log(this.innerHTML);
+     this.style.color = "#fff";
+
+   });
 }
 
 function handleClick() {
-  alert("I got clicked");
+  var tom_1 = new Audio('sounds/tom-1.mp3');
+  tom_1.play();
 }
