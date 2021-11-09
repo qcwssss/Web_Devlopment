f6121e127153f43615542310954742815120ff15
Chen Qiu <qiu.chen1@northeastern.edu>
2021-11-09 09:40:10 -0800

    update select

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,1 +1,17 @@
 $("h1").addClass("big-title margin-50");
+console.log($("img").attr("src"));
+
+// eventListener
+$("button").click(function(){
+  $("h1").css("color", "red");
+});
+
+// key input
+$("input").keydown(function(event) {
+  //console.log(event.key);
+  $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function() {
+  $("h1").css("color", "blue");
+});
