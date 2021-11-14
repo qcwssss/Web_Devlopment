500c838eb00eb20b6b9368fe49636b49cdda8877
Chen Qiu <qiu.chen1@northeastern.edu>
2021-11-14 11:29:03 -0800

    complete weather app

--- Apps/WeatherProject/app.js
+++ Apps/WeatherProject/app.js
@@ -1,27 +1,44 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res) {
+app.use(bodyParser.urlencoded({extended:true}));
 
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=6b8c6d85a30bf07e604b420c93afbc25&units=metric";
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+
+  const query = req.body.cityName;
+  const apiKey = "0b36f3c8a84b21ae28446442f860be8d";
+  const unit = "metric";
+
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid="+ apiKey +"&units=" + unit;
   https.get(url, function(response) {
     console.log(response.statusCode);
 
     response.on("data", function(data) {
       var weatherData = JSON.parse(data);
-      console.log(weatherData);
 
-      const ramen = {
-        name: "Angela",
-        favorite:"Ramen"
-      }
-      console.log(JSON.stringify(ramen));
-    })
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      const icon_url = "https://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png" ;
+
+      res.write("<p>The weather is currently " + description + "</p>");
+      res.write("<h1>The weather in London is " + temp + " degrees Celcius.</h1>");
+      res.write("<img src="+ icon_url +">");
+
+      res.send();
+
+    });
   })
-  res.send("Server is up and running");
-});
+})
+
+
+
 
 app.listen(3000, function(req, res) {
   console.log("Server is running on port 3000");
