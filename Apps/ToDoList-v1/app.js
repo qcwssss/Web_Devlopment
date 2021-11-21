276d7cfbe1c4142e9b9eb4822341f2ad8b496d60
Chen Qiu <qiu.chen1@northeastern.edu>
2021-11-21 15:41:10 -0800

    complete todo list v1

--- Apps/ToDoList-v1/app.js
+++ Apps/ToDoList-v1/app.js
@@ -1,26 +1,28 @@
+//jshint esversion: 6
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
-require('dotenv').config; // hide info
+require('dotenv').config(); // hide info
 
 // global variable
-var items = ["Buy Food", "Eat Food", "Cook Food"];
+let items = ["Buy Food", "Eat Food", "Cook Food"];
 
 app.get("/", function(req, res) {
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
     kindOfDay: day,
@@ -35,7 +37,7 @@ app.post("/", function(req, res) {
 
   // console.log(todo_next);
   res.redirect("/");
-})
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
