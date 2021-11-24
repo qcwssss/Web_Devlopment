3068af64be385648a1a3b9eb987d4876105b0209
Chen Qiu <qiu.chen1@northeastern.edu>
2021-11-24 12:15:47 -0800

    convert postTitle and params.title to lower case

--- Apps/ejs-challenge/app.js
+++ Apps/ejs-challenge/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -44,6 +45,22 @@ app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
+// get route with parameters
+app.get("/posts/:title", function(req, res) {
+  posts.forEach(function(post) {
+    const postName = req.params.title;
+    // console.log(lodash.lowerCase(postName));
+    // console.log("name: " + postName);
+    // console.log("postTitle: " + post.postTitle);
+
+    if (lodash.lowerCase(post.postTitle) === lodash.lowerCase(postName) ) {
+      console.log("Match Found!");
+    } else {
+      console.log("Not a match");
+    }
+  });
+});
+
 
 // post
 app.post("/compose", function(req, res) {
@@ -53,8 +70,6 @@ app.post("/compose", function(req, res) {
   };
   posts.push(postContent);
   res.redirect("/");
-
-  console.log(posts);
 
 });
 
