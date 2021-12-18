009a09673760a589f960f45f553e75883bc48010
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-17 20:33:59 -0800

    update prototype

--- JS-crash/oop/main.js
+++ JS-crash/oop/main.js
@@ -3,14 +3,14 @@ function Person(firstName, lastName, dob) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.dob = new Date(dob);
+}
 
-    this.getBirthYear = function() {
-        return this.dob.getFullYear();
-    }
+Person.prototype.getBirthYear = function() {
+    return this.dob.getFullYear();
+}
 
-    this.getFullName = function() {
-        return `${this.firstName} ${this.lastName}`;
-    }
+Person.prototype.getFullName = function() {
+    return `${this.firstName} ${this.lastName}`;
 }
 
 // Instantiate object
@@ -18,3 +18,4 @@ const person1 = new Person('John', 'Doe', '4-3-1999');
 
 console.log(person1.getBirthYear());
 console.log(person1.getFullName());
+console.log(person1);
