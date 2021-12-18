7c2a6b83e2632c81bb7795e5bc75178b7ee9e6cb
Chen Qiu <qiu.chen1@northeastern.edu>
2021-12-17 16:13:14 -0800

    switch

--- JS-crash/module1/main.js
+++ JS-crash/module1/main.js
@@ -69,5 +69,27 @@ const todoCompleted = todos.filter(function(elt) {
 
 console.log(todoCompleted);
 
+const x = 10;
+
+if (x === 10) {
+    console.log('x is 10');
+} else {
+    console.log('x is NOT 10');
+}
+
+const color = x > 10 ? 'red' : 'blue';
+console.log(color);
+
+switch(color) {
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'blue':
+        console.log('color is blue');
+        break;
+    default:
+        console.log('color is NOT red or blue');
+        break;    
+}
 
 
